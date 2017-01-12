98b5c5d5920d3c46b5e59bf83966642d85386e3c
Juliane Blier <blier.juliane@gmail.com>
2017-01-12 10:46:28 +0100

    Ajout de la base html

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php 
 	$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
 	$dico = explode("\n", $string);
+ ?><!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Php Crunchings</title>
+ </head>
+ <body>
 
-
-
- ?>
+ 	<h1>Exercices Dictionnaire</h1>
+ 	<div>Ce dictionnaire contient  mots</div>
+ 	<div>Il y a  mots qui font 15 caractères</div>
+ 	<div>Il y a  mots qui contiennent la lettre w</div>
+ 	<div>Il y a  mots qui contiennent la lettre q</div>
+ 	
+ </body>
+ </html>
