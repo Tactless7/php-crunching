0b1719c73af60eeda2ee6267f199d9e203ef157b
Juliane Blier <blier.juliane@gmail.com>
2017-01-12 11:10:41 +0100

    Structure HTML

---
+++ films.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Php Crunchings</title>
+ </head>
+ <body>
+
+ 	<h1>Exercices Films</h1>
+ 	<div>
+ 		<h2>Top 10 des films</h2>
+ 	</div>
+ 	<div>Classement du film Gravity : </div>
+ 	<div>Réalisateur de The Lego Movie</div>
+ 	<div>Nombre de films sortis avant 2000 : </div>
+ 	<div>Film le plus récent : </div>
+ 	<div>Film le plus vieux : </div>
+ 	<div>Catégorie de film la plus représentée : </div>
+ 	<div>Réalisateur le plus présent dans le top 100</div>
+ 	<div>Prix d'achat du Top 10 sur itunes : </div>
+ 	<div>Prix de location du Top 10 sur itunes : </div>
+ 	<div>Mois avec le plus de sorties au cinémat : </div>
+ 	<div>Les 1à meilleurs films à voir avec un petit budget : </div>
+
+ 	
+ </body>
+ </html>
